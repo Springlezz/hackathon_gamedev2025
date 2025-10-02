49d00d45a7b346fa2679a3d5dd9cd4e1c81e3bea
Mikhail Zuenko <zuenko.michail@gmail.com>
2025-10-03 06:42:28 +0700

    Сущности для кислоты

--- scripts/entities/acid-flow.js
+++ scripts/entities/acid-flow.js
@@ -2,10 +2,10 @@ import Texture from '../texture.js';
 import Vector2 from '../vector2.js';
 import Entity from './entity.js';
 
-const texture = await Texture.load('acid');
+const texture = await Texture.load('acid-flow');
 
-export default class Acid extends Entity {
+export default class AcidFlow extends Entity {
     texture = texture;
-    size = new Vector2(64, 30);
+    size = new Vector2(20, 64);
     solid = false;
 }
