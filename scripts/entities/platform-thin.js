ef3c843cacb959999e85bc4cb4757318afeb485b
Mikhail Zuenko <zuenko.michail@gmail.com>
2025-10-02 13:38:56 +0700

    Платформы разного размера

--- scripts/entities/platform-thin.js
+++ scripts/entities/platform-thin.js
@@ -6,14 +6,30 @@ const texture = await Texture.load('platform-thin');
 
 export default class PlatformThin extends Entity {
     texture = texture;
-    size = new Vector2(64, 16);
 
-    constructor(position, vertical = false) {
+    constructor(position, length = 1, vertical = false) {
         super(position);
+        this.length = length;
         this.vertical = vertical;
+
+        if (vertical) this.size = new Vector2(16, this.length * 64);
+        else this.size = new Vector2(this.length * 64, 16);
     }
 
     render(ctx, dt) {
-        this.texture.render(ctx, dt, this.position, this.vertical ? Math.PI / 2 : 0);
+        if (this.vertical) {
+            let offset = new Vector2(0, (1 - 1 / this.length) * this.size.y / 2);
+            for (let i = 0; i < this.length; ++i) {
+                this.texture.render(ctx, dt, this.position.clone().add(offset), Math.PI / 2);
+                offset.y -= this.size.y / this.length;
+            }
+        }
+        else {
+            let offset = new Vector2((1 - 1 / this.length) * this.size.x / 2, 0);
+            for (let i = 0; i < this.length; ++i) {
+                this.texture.render(ctx, dt, this.position.clone().add(offset));
+                offset.x -= this.size.x / this.length;
+            }
+        }
     }
 }
