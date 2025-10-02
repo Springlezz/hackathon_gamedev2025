847048d23ccb8c9ed7a0dd87582186801180bc99
Mikhail Zuenko <zuenko.michail@gmail.com>
2025-10-02 18:31:01 +0700

    Второй level

--- scripts/levels/level2.js
+++ scripts/levels/level2.js
@@ -8,28 +8,34 @@ import BatterySlot from '../entities/battery-slot.js';
 import Button from '../entities/button.js';
 import Ladder from '../entities/ladder.js';
 import LaserStation from '../entities/laser-station.js';
+import Texture from '../texture.js';
 
+const backgroundTexture = await Texture.load('backgrounds/1');
 
 export default class extends Level {
+    background = backgroundTexture;
     startPoint = new Vector2(-448, -224);
     endPoint = new Vector2(448, -224);
 
     platforms = [
         new BorderBottomFull(new Vector2(0, -272)),
- 
+
         new Platform(new Vector2(-150, -40), 2), //справа
         new Platform(new Vector2(150, -100), 2), //слева
 
         new PlatformThin(new Vector2(-400, 192), 2, true), //3 up, убирающееся панель
         new Button(new Vector2(-473, 180), false), //слева на стене кнопка
         new Button(new Vector2(266, 248), true), //справа наверху кнопка
-        
-        new Ladder(new Vector2(-280, -32)),
-        new Ladder(new Vector2(310, -90)),
+
+
 
         new LaserStation(new Vector2(-30, 248)),
         new LaserStation(new Vector2(30, 248)),
 
-        
+
+    ];
+    ladders = [
+        new Ladder(new Vector2(-280, -32), 5),
+        new Ladder(new Vector2(310, -90), 5)
     ];
 }
